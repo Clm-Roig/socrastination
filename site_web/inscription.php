e85d660f32fb8165bf15b4aad42f7b5af6a41af4
ferrerm <maxime.ferrer10@gmail.com>
2016-03-08 18:42:37 +0000

    Test neméro 3 (j'arrive pas a tester en local)

--- site_web/inscription.php
+++ site_web/inscription.php
@@ -57,7 +57,7 @@
 
 			<div class="col-sm-8">
 
-				 <form class="form-horizontal">
+				 <form class="form-horizontal" role="form" action="cible.php" method="post">
 					<h1>Inscription</h1>
 
 					  <div class="form-group">
@@ -99,24 +99,7 @@
 						    </div>
 					  </div>
 				</form>
-					<?php
-					$PARAM_hote='http://infolimon.iutmontp.univ-montp2.fr/my'; // le chemin vers le serveur
-					$PARAM_nom_bd='chacc'; // le nom de votre base de données
-					$PARAM_utilisateur='chacc'; // nom d'utilisateur pour se connecter
-					$PARAM_mot_passe='PetitChatDu34'; // mot de passe de l'utilisateur pour se connecter
-					
-					$bdd= new PDO('mysql:host=http://infolimon.iutmontp.univ-montp2.fr/my;dbname=chacc;charset=utf8','chacc','PetitChatDu34');
-					
-					// REQUETE 
-					$req = 'INSERT INTO Membre(pseudo, motDePasse, mail) VALUES(:pseudo, :motDePasse, :mail);';
-					$stmt = $bdd->prepare($req);
 
-					// PDO::BINDPARAM
-					$stmt->bindParam(':pseudo', $_POST['pseudo'], PDO::PARAM_STR);
-					$stmt->bindParam(':motDePasse', $_POST['motDePasse'], PDO::PARAM_STR);
-					$stmt->bindParam(':mail', $_POST['mail'], PDO::PARAM_STR);
-					$stmt->execute() or die (print_r($stmt->errorInfo(), true));
-					?>
 			</div>
 
 			<div class="col-sm-2"></div>
