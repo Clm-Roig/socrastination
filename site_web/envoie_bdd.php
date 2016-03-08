c15a82357416c77a888f88ce0da7d64e04e90d7f
ferrerm <maxime.ferrer10@gmail.com>
2016-03-08 18:47:44 +0000

    test 6 bdd

--- site_web/envoie_bdd.php
+++ site_web/envoie_bdd.php
@@ -3,9 +3,11 @@
 					$PARAM_nom_bd='chacc'; // le nom de votre base de données
 					$PARAM_utilisateur='chacc'; // nom d'utilisateur pour se connecter
 					$PARAM_mot_passe='PetitChatDu34'; // mot de passe de l'utilisateur pour se connecter
-					
+					try{
 					$bdd= new PDO('mysql:host=http://infolimon.iutmontp.univ-montp2.fr/my;dbname=chacc;charset=utf8','chacc','PetitChatDu34');
-					
+					} catch (PDOException $e) {
+					echo 'Erreur de connexion : ' . $e->getMessage();
+					}
 					// REQUETE 
 					$req = 'INSERT INTO Membres(pseudo, motDePasse, mail) VALUES(:pseudo, :motDePasse, :mail);';
 					$stmt = $bdd->prepare($req);
