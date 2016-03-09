ffcda813a872136628d896d62debeb3b92b51672
ferrerm <maxime.ferrer10@gmail.com>
2016-03-09 13:26:19 +0000

    page inscription fini

--- site_web/envoie_bdd.php
+++ site_web/envoie_bdd.php
@@ -1,17 +1,20 @@
 					<?php
-					$PARAM_hote='localhost'; // le chemin vers le serveur
+					$PARAM_hote='http://infolimon.iutmontp.univ-montp2.fr/'; // le chemin vers le serveur
 					$PARAM_nom_bd='chacc'; // le nom de votre base de données
 					$PARAM_port='3306';
 					$PARAM_utilisateur='chacc'; // nom d'utilisateur pour se connecter
 					$PARAM_mot_passe='PetitChatDu34'; // mot de passe de l'utilisateur pour se connecter
 					try{
-					$bdd = new PDO('mysql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
-					} catch (PDOException $e) {
-					echo 'Erreur de connexion : ' . $e->getMessage();
-					}
+					$bdd = new PDO ('mysql:dbname=chacc;host=infolimon.iutmontp.univ-montp2.fr','chacc','PetitChatDu34');}
+					  //new PDO('mysql:dbname='.$PARAM_nom_bd.';host='.$PARAM_hote, $PARAM_utilisateur, $PARAM_mot_passe);
+					
+catch (PDOException $e){
+       echo "problème PDO :". $e->getMessage();
+       exit();
+   }
 					try{
 					// REQUETE 
-					$req = 'INSERT INTO Membres(pseudo, motDePasse, mail) VALUES(:pseudo, :motDePasse, :mail);';
+					$req = 'INSERT INTO Membres(pseudo, motDePasse, mail, niveau, nbDePoints, nbPartiesGagnees, NbTotalParties) VALUES(:pseudo, :motDePasse, :mail, 1, 0, 0, 0);';
 					$stmt = $bdd->prepare($req);
 
 					// PDO::BINDPARAM
@@ -22,4 +25,9 @@
 					} catch (PDOException $e) {
     echo 'Erreur enregistrement des donnees : ' . $e->getMessage();
 }
+echo 'Inscription reussis, vous allez etre redirige sur la page inscription automatiquement';
+echo 'Sinon clique <a href="inscription.php">ici</a>';
+      
+header("refresh:3;url=inscription.php");
+ 
 					?>
