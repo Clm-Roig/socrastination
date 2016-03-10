c0766f2e6b1a0a80771b741ec3aeb3f2cd99e6d1
clm.roig <clm.roig@gmail.com>
2016-03-10 10:01:41 +0000

    Ajout du fichier testinterfacejeu.php pour tester le chat progressivement

---
+++ site_web/testinterfacejeu.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<link rel="icon" href="images/favicon.ico"/>
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+
+	<!-- CSS -->	
+
+		<!-- BOOTSTRAP -->
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+		<!-- PERSO -->
+		<link rel="stylesheet" href="css/style.css">   
+
+	<!-- === -->
+
+	<!-- POLICES EN LIGNE -->
+	<link href='https://fonts.googleapis.com/css?family=Kanit' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Orbitron:400,700' rel='stylesheet' type='text/css'>
+	<!-- ================ -->
+	
+	<title>Socrat' - Jeu</title>
+   	
+</head>
+
+<body> 
+
+	<div class="row">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-12" id="headerjeu">
+					<h2>en haut du jeu</h2>
+				</div>
+			</div>
+
+	<!-- INTERFACE DE CHAT -->
+			<div class="row">
+
+				<div class="col-sm-1"></div>
+				<div class="col-sm-10">
+
+					<div id="conversation"></div>
+					<div class="row">
+						<div class="col-sm-12">
+
+							 <form class="form-inline" id="form_chat">
+								<div class="form-group">
+									<input type="text" class="form-control" id="message" placeholder="Message...">
+								</div>
+								  <button type="submit" class="btn btn-default">Envoyer</button>
+							</form>
+
+						</div>
+					</div>
+
+				</div>
+				<div class="col-sm-1"></div>
+			</div>
+	<!-- FIN INTERFACE CHAT -->
+
+			<div class="row">
+				<div class="col-sm-12"><hr class="foot"></div>
+				<footer>
+					<p>Mentions légales 2016, IUT Montpellier-Sète - Projet AS : Dalle-Cort, Chac, Ferrer, Roig</p>
+				</footer>
+			</div>
+
+	  	</div><!-- Ferme ROW principale -->
+	</div><!-- Ferme CONTAINER-FLUID principale -->
+	
+
+</body>
+</html>
