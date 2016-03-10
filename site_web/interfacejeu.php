a511c271a15a29214e2411596a0c592820a33beb
clm.roig <clm.roig@gmail.com>
2016-03-10 09:37:37 +0000

    Fichier interfacejeu.php vidé en préparation de la conception du chat

--- site_web/interfacejeu.php
+++ site_web/interfacejeu.php
@@ -37,86 +37,35 @@
 <body> 
 
 	<div class="row">
-	  <div class="container-fluid">
-
-		<div class="row">
-			<div class="col-sm-12" id="headerjeu">
-				<h2>en haut du jeu</h2>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-12" id="headerjeu">
+					<h2>en haut du jeu</h2>
+				</div>
 			</div>
-		</div>
-	
-		<div class="row">
-			<div class="col-sm-12" id="interface">
-				<div class="vs">VS</div>
-						<div class="col-sm-2"></div>
-								<div class="row" id="barresdevie">
-										<div class="col-sm-12">
-												<div class="col-sm-6" id="barrevie1">
-													Barre de vie 1
-												</div>
-
-												<div class="col-sm-6" id="barrevie2">
-													Barre de vie 2
-												</div>
-										</div>
-								</div>
-						<div class="col-sm-2"></div>
-
-					<div class="row">
-							<div class="col-sm-12" id="chat">
-								<div class="row" id="chat1">	
-										<p class="chat">Salut, j'aimerai te soumettre mon premier argument à propos de ce sujet qui me tient particulièrement à coeur. Alors tout d'abord, sache que la contradiction stéréopolaire entre en corrélation indirecte avec les cieux.</p>
-								</div>
-
-								<div class="row" id="chat2">
-										<p class="chat">Ah oui d'accord, je vois.</p>
-								</div>
-
-								<div class="row" id="chat1">
-									<p class="chat">C'est bien beau de voir mais est-ce que tu comprends au moins ? Sinon je peux détailler hein...</p>
-					
-								</div>
-
-								<div class="row" id="chat2">
-										<p class="chat">Mais oui t'inquiète. T'es mignon tu sais ?</p>
-								</div>
-
-							</div>
-					</div>
-
+			<div class="row">
+				<div class="col-sm-12" id="interface">
+					<div class="vs">Jammy VS Fred</div>
 						<div class="row">
-							<div class="col-sm-12">	
-								<div class="col-sm-1"></div>
-
-								<div class="col-sm-1 tete">O   O</div>
-								<div class="col-sm-1 tete">O   O</div>
-								<div class="col-sm-1 tete">O   O</div>
-								<div class="col-sm-1 tete">O   O</div>
-								<div class="col-sm-1 tete">O   O</div>
-								<div class="col-sm-1 tete">O   O</div>
-								<div class="col-sm-1 tete">O   O</div>
-								<div class="col-sm-1 tete">O   O</div>
-								<div class="col-sm-1 tete">O   O</div>
-								<div class="col-sm-1 tete">O   O</div>
-
-								<div class="col-sm-1"></div>
+							<div class="col-sm-12" id="chat">
+	
 							</div>
 						</div>
-
+					</div>
 				</div>
 			</div>
 		
-
 			<div class="row">
 				<div class="col-sm-12"><hr class="foot"></div>
-
 				<footer>
 					<p>Mentions légales 2016, IUT Montpellier-Sète - Projet AS : Dalle-Cort, Chac, Ferrer, Roig</p>
 				</footer>
 			</div>
 
-	  </div>
+	  	</div>
 	</div>
+
+	
 
 </body>
 </html>
