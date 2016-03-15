ab6e3d2e330a41e1e47baf82b823beef08ec44fe
clm.roig <clm.roig@gmail.com>
2016-03-15 09:10:59 +0000

    Nettoyage du code de liste_forums.php, ajout de boutons

--- site_web/liste_forums.php
+++ site_web/liste_forums.php
@@ -30,103 +30,110 @@
 	<link href='https://fonts.googleapis.com/css?family=Orbitron:400,700' rel='stylesheet' type='text/css'>
 	<!-- ================ -->
 	
-	<title>Socrat' - Accueil</title>
+	<title>Socrat' - Liste des Forums</title>
    	
 </head>
 
 <body> 
 
-     <div class="row">
-	<div class="container-fluid">
+    	<!-- DIV GLOBALE -->
+	<div class="row" id="row_corps">
+	<!-- ============ -->
 
+		<!-- HEADER / NAV -->
 		<?php include("elements_communs/header1.php");?>
-			<ul class="nav navbar-nav">
-							<li class="menu"><a href="index.php">Accueil</a></li>
-							<li class="menu"><a href="presentation.php">Présentation</a></li>
-							<li class="menu"><a href="regles.php">Règles</a></li>
-							<li class="menu"><a href="classement.php">Classement</a></li>
-					      </ul>
+		<ul class="nav navbar-nav">
+			<li class="menu"><a href="index.php">Accueil</a></li>
+			<li class="menu"><a href="presentation.php">Présentation</a></li>
+			<li class="menu"><a href="regles.php">Règles</a></li>
+			<li class="menu"><a href="classement.php">Classement</a></li>
+		</ul>
 
-					      <ul class="nav navbar-nav navbar-right">
-							<li><a href="inscription.php"><span class="glyphicon glyphicon-user"></span> S'inscrire</a></li>
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="inscription.php"><span class="glyphicon glyphicon-user"></span> S'inscrire</a></li>
 		<?php include("elements_communs/header2.php");?>
+		<!-- ============ -->
+
+		<div class="container" id="corps">
 
 			<div class="row">
 				<div class="col-sm-12">
-						<table class="table table-hover table-responsive" id="liste_forums">	
-							    <thead>
-								      <tr>
-									<th>N° Forum</th>
-									<th>Nombre de joueurs</th>
-									<th>Nombre d'arbitres</th>
-								      </tr>
-							    </thead>
-							    <tbody>
-								      <tr>
-									<td>1</td>
-									<td>0/2</td>
-									<td>0/10</td>
-								      </tr>
-								      <tr>
-									<td>2</td>
-									<td>0/2</td>
-									<td>0/10</td>
-								      </tr>
-								      <tr>
-									<td>3</td>
-									<td>0/2</td>
-									<td>0/10</td>
-								      </tr>
-								      <tr>
-									<td>4</td>
-									<td>0/2</td>
-									<td>0/10</td>
-								      </tr>
-								      <tr>
-									<td>5</td>
-									<td>0/2</td>
-									<td>0/10</td>
-								      </tr>
-								      <tr>
-									<td>6</td>
-									<td>0/2</td>
-									<td>0/10</td>
-								      </tr>
-								      <tr>
-									<td>7</td>
-									<td>0/2</td>
-									<td>0/10</td>
-								      </tr>
-								      <tr>
-									<td>8</td>
-									<td>0/2</td>
-									<td>0/10</td>
-								      </tr>
-								      <tr>
-									<td>9</td>
-									<td>0/2</td>
-									<td>0/10</td>
-								      </tr>
-								      <tr>
-									<td>10</td>
-									<td>0/2</td>
-									<td>0/10</td>
-								      </tr>
-							    </tbody>
-						</table>
+					<table class="table table-hover table-responsive" id="liste_forums">	
+						<thead>
+							<tr>
+								<th>N° Forum</th>
+								<th>Nombre de joueurs</th>
+								<th>Nombre d'arbitres</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>0/2 <button class="btn btn-default">Rejoindre en tant que joueur</button></td>
+								<td>0/10 <button class="btn btn-default">Rejoindre en tant qu'arbitre</button></td>
+							</tr>
+						      	<tr>
+								<td>2</td>
+								<td>0/2 <button class="btn btn-default">Rejoindre en tant que joueur</button></td>
+								<td>0/10 <button class="btn btn-default">Rejoindre en tant qu'arbitre</button></td>
+							</tr>
+							<tr>
+								<td>3</td>
+								<td>0/2 <button class="btn btn-default">Rejoindre en tant que joueur</button></td>
+								<td>0/10 <button class="btn btn-default">Rejoindre en tant qu'arbitre</button></td>
+							</tr>
+							<tr>
+								<td>4</td>
+								<td>0/2 <button class="btn btn-default">Rejoindre en tant que joueur</button></td>
+								<td>0/10 <button class="btn btn-default">Rejoindre en tant qu'arbitre</button></td>
+							</tr>
+							<tr>
+								<td>5</td>
+								<td>0/2 <button class="btn btn-default">Rejoindre en tant que joueur</button></td>
+								<td>0/10 <button class="btn btn-default">Rejoindre en tant qu'arbitre</button></td>
+							</tr>
+							<tr>
+								<td>6</td>
+								<td>0/2 <button class="btn btn-default">Rejoindre en tant que joueur</button></td>
+								<td>0/10 <button class="btn btn-default">Rejoindre en tant qu'arbitre</button></td>
+							</tr>
+							<tr>
+								<td>7</td>
+								<td>0/2 <button class="btn btn-default">Rejoindre en tant que joueur</button></td>
+								<td>0/10 <button class="btn btn-default">Rejoindre en tant qu'arbitre</button></td>
+							</tr>
+							<tr>
+								<td>8</td>
+								<td>0/2 <button class="btn btn-default">Rejoindre en tant que joueur</button></td>
+								<td>0/10 <button class="btn btn-default">Rejoindre en tant qu'arbitre</button></td>
+							</tr>
+							<tr>
+								<td>9</td>
+								<td>0/2 <button class="btn btn-default">Rejoindre en tant que joueur</button></td>
+								<td>0/10 <button class="btn btn-default">Rejoindre en tant qu'arbitre</button></td>
+							</tr>
+							<tr>
+								<td>10</td>
+								<td>0/2 <button class="btn btn-default">Rejoindre en tant que joueur</button></td>
+								<td>0/10 <button class="btn btn-default">Rejoindre en tant qu'arbitre</button></td>
+							</tr>
+						</tbody>
+					</table>
+ 
 				</div>
 			</div>
 
-			<div class="row">
-				<div class="col-sm-12"><hr class="foot"></div>
+		</div><!-- Ferme le bloc du milieu -->
 
-				<footer>
-					<p>Mentions légales 2016, IUT Montpellier-Sète - Projet AS : Dalle-Cort, Chac, Ferrer, Roig</p>
-				</footer>
+		<!-- FOOTER -->
+		<div class="row">
+			<div class="col-sm-12" id="footer">
+					<p class="foot">Mentions légales 2016, IUT Montpellier-Sète - Projet AS : Dalle-Cort, Chac, Ferrer, Roig</p>
 			</div>
+		</div>
+		<!-- ===== -->
 
-	  </div>
-	</div>
+	</div><!-- Ferme la row_corps" -->
 
 </body>
 </html>
