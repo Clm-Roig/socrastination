99261ae64c6b3ccae563e86aac5116af5179efae
clm.roig <clm.roig@gmail.com>
2016-03-18 13:19:37 +0000

    Indentation du code de membre.php

--- site_web/membre.php
+++ site_web/membre.php
@@ -1,36 +1,25 @@
 <?php
-session_start();
-if (!isset($_SESSION['pseudo'])) {
-	header ('Location: index.php');
-	exit();
-}
+	session_start();
+	if (!isset($_SESSION['pseudo'])) {
+		header ('Location: index.php');
+		exit();
+	}
 	
-	// connexion 
-   require("config.php");
-   
-
-
-
-   // INTERROGATION
-   $query = $bdd->prepare('SELECT * FROM Membres WHERE pseudo = :pseudo');
-   $query->bindValue(':pseudo',$_SESSION['pseudo'], PDO::PARAM_STR);
-   $query->execute();
-   $info=$query->fetchobject();
-   if ($query==false) {
-       echo "erreur query";
-       exit();
-   }
-  
-
-   
-   // TRAITEMENT
-
-       echo "Pseudo : {$info->pseudo} ; Mail : {$info->mail}; Niveau : {$info->niveau}; Points : {$info->nbDePoints}; Parties gagnees : {$info->nbPartiesGagnees}; Total parties : {$info->NbTotalParties} ";
+	// connexion BDD
+	require("config.php");
 	   
+	// INTERROGATION
+	$query = $bdd->prepare('SELECT * FROM Membres WHERE pseudo = :pseudo');
+	$query->bindValue(':pseudo',$_SESSION['pseudo'], PDO::PARAM_STR);
+	$query->execute();
+	$info=$query->fetchobject();
+	if ($query==false) {
+		echo "erreur query";
+		exit();
+	}   
 
-
-	
-	
+   	// TRAITEMENT
+	echo "Pseudo : {$info->pseudo} ; Mail : {$info->mail}; Niveau : {$info->niveau}; Points : {$info->nbDePoints}; Parties gagnees : {$info->nbPartiesGagnees}; Total parties : {$info->NbTotalParties} ";	
 	
 ?>
 
