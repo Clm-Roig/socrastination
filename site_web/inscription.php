12d94d3d2deb711987ebe115893c151994ffa4eb
clm.roig <clm.roig@gmail.com>
2016-03-18 13:25:34 +0000

    Esthétique messages de contrôle du formulaire

--- site_web/inscription.php
+++ site_web/inscription.php
@@ -98,16 +98,23 @@
 								    	<div class="col-sm-9">
 								      		<input type="password" required="required" class="form-control" id="pwdconfirm" placeholder="Confirmer mot de passe" onkeyup="checkpass();">
 								    	</div>
-							
-									<div id="alerte"></div>
+			
 							  	</div>
 							</div>
-					
-						  	<div class="form-group">
-								<div class="col-sm-12">
-							      		<button type="submit" class="btn btn-default">Envoyer</button>
-							    	</div>
-						  	</div>
+
+							<div class="row">
+								<div class="form-group">
+									<div id="alerte" class="col-sm-12"></div>
+								</div>
+							</div>
+							
+							<div class="row">
+							  	<div class="form-group">
+									<div class="col-sm-12">
+								      		<button type="submit" class="btn btn-default">Envoyer</button>
+								    	</div>
+							  	</div>
+							</div>
 
 						</form>
 
@@ -136,10 +143,10 @@
 			var val2 = document.getElementById("pwdconfirm");
 		 
 		 	 if(val1.value != val2.value){
-				document.getElementById("alerte").innerHTML="Tapez deux mots de passe identiques, merci.";
+				document.getElementById("alerte").innerHTML="<p class=\"text-danger\">Tapez deux mots de passe identiques, merci.</p>";
 		  	}
 		 	else{
-		    		document.getElementById("alerte").innerHTML="Mot de passe valide, bravo !";
+		    		document.getElementById("alerte").innerHTML="<p class=\"text-success\">Mot de passe valide, bravo !</p>";
 		  	}
 		}
 	</script>
