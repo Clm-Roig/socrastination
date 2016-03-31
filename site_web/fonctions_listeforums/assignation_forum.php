4cf939cc21c0aa15122ec1a03fe717652b091d03
clm.roig <clm.roig@gmail.com>
2016-03-31 08:20:35 +0000

    Maj assignation, finir l'obtention du num forum

--- site_web/fonctions_listeforums/assignation_forum.php
+++ site_web/fonctions_listeforums/assignation_forum.php
@@ -9,10 +9,17 @@
 	}
 	
 	else {
-		//REQUETE SQL => Insertion dans Role
-		$req = 'INSERT INTO Role(role, , ) VALUES(:pseudo, :motDePasse, :mail, 1, 0, 0, 0);';
-		$stmt = $bdd->prepare($req);
+		//RECUPERATION DES VARIABLES
+		$num_forum = $_POST['assign']/10;
+		$num_role = $_POST['assign']%10;
 
-		echo 'Valeur du post : '.$_POST['assign'];
+		echo 'Valeur du num_forum : '.$num_forum;
+		echo '<br>Valeur du role : '.$num_role;
+		/*
+		$res_partie_creee = 'SELECT' ;
+		$req = 'INSERT INTO Role(role, , ) VALUES(:pseudo, :motDePasse, :mail, 1, 0, 0, 0);';
+		$stmt = $bdd->prepare($req);*/
+
+		echo '<br>Valeur du post : '.$_POST['assign'];
 	}
 ?>
