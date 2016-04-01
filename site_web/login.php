f98d0efc33f373a9a229367c06d195ea4af70f10
clm.roig <clm.roig@gmail.com>
2016-04-01 10:26:28 +0000

    Ajout d'une case dans la supervariable SESSION => id membre

--- site_web/login.php
+++ site_web/login.php
@@ -7,12 +7,13 @@
 		exit();
 	}
 	else { //On check le mot de passe
-		$query=$bdd->prepare('SELECT pseudo, motDePasse FROM Membres WHERE pseudo = :pseudo');
+		$query=$bdd->prepare('SELECT idMembre,pseudo, motDePasse FROM Membres WHERE pseudo = :pseudo');
 		$query->bindValue(':pseudo',$_POST['pseudo'], PDO::PARAM_STR);
 		$query->execute();
 		$data=$query->fetch();
 		if ($data['motDePasse'] == $_POST['pwd']) { // Accès OK 
 			$_SESSION['pseudo'] = $data['pseudo'];
+			$_SESSION['idMembre'] = $date['idMembre'];
 			header('Location: membre.php'); 
 		}
 		else { // Accès pas OK !
