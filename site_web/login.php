59650ce70e1a8e905cfdd2d26fd8e28db05e9311
ferrerm <maxime.ferrer10@gmail.com>
2016-04-07 12:47:55 +0000

    maj login

--- site_web/login.php
+++ site_web/login.php
@@ -9,17 +9,21 @@
 	else { //On check le mot de passe
 		$query=$bdd->prepare('SELECT idMembre,pseudo, motDePasse FROM Membres WHERE pseudo = :pseudo');
 		$query->bindValue(':pseudo',$_POST['pseudo'], PDO::PARAM_STR);
-		$query->execute();
-		$data=$query->fetch();
-		if ($data['motDePasse'] == $_POST['pwd']) { // Accès OK 
-			$_SESSION['pseudo'] = $data['pseudo'];
-			$_SESSION['idMembre'] = $data['idMembre'];
-			header('Location: membre.php'); 
+		if($query->execute()){
+			$data=$query->fetch();
+			if ($data['motDePasse'] == $_POST['pwd']) { // Accès OK 
+				$_SESSION['pseudo'] = $data['pseudo'];
+				$_SESSION['idMembre'] = $data['idMembre'];
+				header('Location: membre.php'); 
+			}
+			else { // Accès pas OK !
+				echo "Erreur login, mot de passe ou pseudo incorect !";
+			}
+			$query->CloseCursor();
 		}
-		else { // Accès pas OK !
-			echo "Erreur login, mot de passe ou pseudo incorect !";
+		else { 
+			header('Location: erreur_paslog.php?num_erreur=1');
 		}
-	    $query->CloseCursor();
-	    }
+	}
 
 ?>
