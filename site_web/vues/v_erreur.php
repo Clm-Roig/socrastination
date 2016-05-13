0d337a97224c2d760b7a231b4bd2cb9c750f961a
clm.roig <clm.roig@gmail.com>
2016-05-13 13:42:21 +0000

    maj erreur MVC

---
+++ site_web/vues/v_erreur.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+
+	<link rel="icon" href="images/favicon.ico"/>
+
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+
+	<!-- CSS -->	
+
+		<!-- BOOTSTRAP -->
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+		<!-- PERSO -->
+		<link rel="stylesheet" href="css/style.css">   
+
+	<!-- === -->
+
+	<!-- POLICES EN LIGNE -->
+	<link href='https://fonts.googleapis.com/css?family=Kanit' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Orbitron:400,700' rel='stylesheet' type='text/css'>
+	<!-- ================ -->
+	
+	<title>Socrastination - Erreur</title>
+   	
+</head>
+
+<body>
+
+    	<!-- DIV GLOBALE -->
+	<div class="row" id="row_corps">
+	<!-- ============ -->
+
+		<!-- HEADER / NAV -->
+	<div class="row">
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div id="myNavbar">
+
+					<ul class="nav navbar-nav">
+						<li class="menu"><a href="index.php">Accueil</a></li>
+						<li class="menu"><a href="regles.php">Règles</a></li>
+						<li class="menu"><a href="classement.php">Classement</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>	
+		<!-- ============ -->
+
+		<div class="container" id="corps">
+			<div class='row'>
+				<h1>{erreur}</h1>	
+			</div>
+		</div><!-- Ferme le bloc du milieu --> 
+
+		<!-- FOOTER -->
+		<div class="row">
+			<div class="col-sm-12" id="footer">
+				<p class="foot">Mentions légales 2016, IUT Montpellier-Sète - Projet AS : Dalle-Cort, Chac, Ferrer, Roig</p>
+			</div>
+		</div>
+		<!-- ===== -->
+
+	</div><!-- Ferme la row_corps" -->
+</body>
+</html>
+
