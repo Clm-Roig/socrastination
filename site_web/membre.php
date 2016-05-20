c953aea8556d48f949e8567510eb848e8f4bc17c
clm.roig <clm.roig@gmail.com>
2016-05-20 09:21:06 +0000

    Modif d'un lien sur la page membre

--- site_web/membre.php
+++ site_web/membre.php
@@ -60,7 +60,7 @@
 						</ul>
 
 						<ul class="nav navbar-nav navbar-right">
-						<li><a href="inscription.php"><span class="glyphicon glyphicon-user"></span> S'inscrire</a></li>
+						<li><a href="index.php?action=inscription"><span class="glyphicon glyphicon-user"></span> S'inscrire</a></li>
 
 					<?php 
 						if (!isset($_SESSION['pseudo'])) {
