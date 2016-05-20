fcc9aea7da05c308439add3473406f74647c9750
clm.roig <clm.roig@gmail.com>
2016-05-20 09:13:30 +0000

    Classement en MVC : ok

--- site_web/index.php
+++ site_web/index.php
@@ -1,5 +1,7 @@
 <?php
 	session_start();
+	require("config.php");
+
 	if (!isset($_SESSION['pseudo'])) {
 		$header=file_get_contents("elements_communs/header2.php");
 	}
@@ -7,7 +9,51 @@
 		$header=file_get_contents("elements_communs/header3.php");
 	}
 
-	$vue=file_get_contents("vues/v_index.html");
+	if(isset($_GET['action'])) $action=$_GET['action'];
+	else $action='default';
+
+	switch($action) {
+
+/*----------------- CLASSEMENT ----------------- */
+		case("classement") :
+			$vue=file_get_contents("vues/v_classement.html");	
+
+	   		// INTERROGATION 
+	   		$classement = $bdd -> query("SELECT * FROM Membres ORDER BY nbDePoints DESC LIMIT 5");
+	   		if ($classement==false) {
+	       			header('Location: erreur.php?num_erreur=1');
+	       			exit();
+	   		}
+	  
+			$i=1;
+	  		while(($info=$classement -> fetchobject())!=null){ 
+	       			$ps=$info->pseudo; 
+				$po=$info->nbDePoints;
+				$vue=str_replace("{pseudo".$i."}",$ps,$vue);
+				$vue=str_replace("{nbp".$i."}",$po,$vue);
+				$i = $i+1;
+		   	}					
+			break;
+
+/*----------------- REGLES ----------------- */
+		case("regles") :
+
+			break;
+
+/*----------------- INSCRIPTION ----------------- */
+		case("inscription") :
+
+			break;
+
+/*----------------- DEFAUT (INDEX) ----------------- */
+		default :
+
+			break;
+
+
+	}//fin switch
+	//Remplacement header + affichage
+
 	$vue=str_replace("{header}",$header,$vue);
 	echo $vue;
 ?>
