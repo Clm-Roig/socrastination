88eb7b5160ea9912606bdb41a9b22e8ab17b9adc
clm.roig <clm.roig@gmail.com>
2016-05-23 07:25:05 +0000

    MAJ table Votes

--- site_web/fonctions_interfacejeu/envoi_vote.php
+++ site_web/fonctions_interfacejeu/envoi_vote.php
@@ -1,52 +1,43 @@
 <?php
-	session_start();
-	//CONNEXION BDD
-	require("../config.php");
+session_start();
+//CONNEXION BDD
+require("../config.php");
 
-	try{
-		$id_message=$_POST['id_message'];
-		if ($_POST['type_vote']==1) {	//VOTE +1
-			// UPDATE DE LA TABLE
-			$req_vote= 	"UPDATE Chat_messages
-						SET votes_up=votes_up+1
-						WHERE message_id=$id_message;
-						";	
+$id_message=$_POST['id_message'];
+if ($_POST['type_vote']==1) {	//VOTE +1
 
-			$bdd->query($req_vote);
+	// UPDATE DE LA TABLE VOTES
+	$req_vote= 	"INSERT INTO Votes(idArbitre, idMessage, vote) 
+				VALUES({$_SESSION['idMembre']},$id_message, 1)
+				";	
+	$bdd->query($req_vote);	
 
-			//CONTROLE
-			if (!$bdd){
-				echo "Erreur envoi BDD, veuillez réessayer.";
-			}
-
-			//OK 
-			else {
-			
-			}
-		}
-
-		else {	//vote -1
-			// UPDATE DE LA TABLE
-			$req_vote= 	"UPDATE Chat_messages
-						SET votes_down=votes_down+1
-						WHERE message_id=$id_message;
-						";	
-
-			$bdd->query($req_vote);
-
-			//CONTROLE
-			if (!$bdd){
-				echo "Erreur envoi BDD, veuillez réessayer.";
-			}
-
-			//OK 
-			else {
-			
-			}
-
-		}
+	//CONTROLE
+	if (!$bdd){
+		echo "Erreur envoi BDD, veuillez réessayer.";
 	}
-	catch (PDOException $e) {
-		echo 'Erreur connexion BDD : ' . $e->getMessage().'';
+
+	//OK 
+	else {
+	
 	}
+}
+
+else {	//vote -1
+	// UPDATE DE LA TABLE VOTES
+	$req_vote= 	"INSERT INTO Votes(idArbitre, idMessage, vote) 
+				VALUES({$_SESSION['idMembre']},$id_message, -1)
+				";	
+	$bdd->query($req_vote);	
+
+	//CONTROLE
+	if (!$bdd){
+		echo "Erreur envoi BDD, veuillez réessayer.";
+	}
+
+	//OK 
+	else {
+	
+	}
+}
 ?>
