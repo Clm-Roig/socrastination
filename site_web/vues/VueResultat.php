4dd07a5f769613827e719f6a26910e11851a74b1
ferrerm <maxime.ferrer10@gmail.com>
2016-06-03 14:08:00 +0000

    vueresultat

---
+++ site_web/vues/VueResultat.php
@@ -0,0 +1,13 @@
+<?php
+	class VueResultat extends Vue{
+				
+		public function __construct(){
+			parent::__construct("v_pageresultat.html");
+		}
+		public function configurer($motclef, $valeur){
+			
+			
+		}
+	}
+	
+?>
