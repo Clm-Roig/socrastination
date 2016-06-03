b29cfd7c2ce483231358501575d6ecc374a9c211
clm.roig <clm.roig@gmail.com>
2016-06-03 08:35:57 +0000

    rien d'important

--- site_web/fonctions_inscription/check_pseudo.php
+++ site_web/fonctions_inscription/check_pseudo.php
@@ -4,7 +4,7 @@
 
 	$pseudo=$_GET['pseudoinscr'];
 	$req = $bdd->query("SELECT pseudo FROM Membres WHERE pseudo='$pseudo'");
-	$nb_occur= $req->fetch(PDO::FETCH_ASSOC);
+	$nb_occur= $req->fetch();
 	if($nb_occur >= 1) {
 		echo '1';
 	}
