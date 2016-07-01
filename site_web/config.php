96a5aa5e64fd7a0005080b0cb18b22b33caae485
berdal84 <contact@dalle-cort.fr>
2016-07-01 18:45:55 +0200

    Update config.php

--- site_web/config.php
+++ site_web/config.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$bdd = new PDO ('mysql:dbname=chacc;charset=utf8;host=infolimon.iutmontp.univ-montp2.fr','chacc','PetitChatDu34');}
+		$bdd = new PDO ('mysql:dbname=chacc;charset=utf8;host=********','*****','*********');}
 		//new PDO('mysql:dbname='.$PARAM_nom_bd.';host='.$PARAM_hote, $PARAM_utilisateur, $PARAM_mot_passe);
 					
 	catch (PDOException $e){
